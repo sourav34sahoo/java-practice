6c62497d771ef336ea868e63b8fe0ab1778c98d5
sourav sahoo <sahoo34sourav@gmail.com>
2023-05-24 21:17:05 +0530

    ArrayLength Vaeiable,method

---
+++ ArrayLength.java
@@ -0,0 +1,11 @@
+class ArrayLength
+{
+  public static void main(String[]args)
+  {
+  int[] x=new int[24];//length variable
+  System.out.println(x.length);
+  
+  String s="sourva";//Method variable
+  System.out.println(s.length());
+  }
+}
