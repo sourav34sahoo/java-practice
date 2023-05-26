7368631c2292cd62e5d6f2360cde56974589c41f
sourav sahoo <sahoo34sourav@gmail.com>
2023-05-26 19:26:32 +0530

    if,else condition

---
+++ Condition.java
@@ -0,0 +1,12 @@
+class Condition
+{
+    public static void main(String[]args){
+        int age = 18;
+        if (age>=18){
+            System.out.println("yes the people can drive!");
+        }
+        else{
+            System.out.println("no the people cannot drive!");
+        }
+    }
+}
