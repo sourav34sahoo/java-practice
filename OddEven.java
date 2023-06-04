ea8f9b6a0a931d4d921a62f6d3e50cdf41cd95e5
sourav sahoo <sahoo34sourav@gmail.com>
2023-06-04 14:04:04 +0530

    Find OddEven numbers

---
+++ OddEven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class OddEven
+{
+  public static void main(String[]args)
+  {
+  Scanner sc= new Scanner(System.in);   
+  System.out.print("Enter first number- ");  
+  int a= sc.nextInt();
+  if (a % 2 ==0){
+  System.out.println("the given numbers are even");
+  }
+  else{
+  System.out.println("the given numbers are odd");
+  }
+  }
+}
