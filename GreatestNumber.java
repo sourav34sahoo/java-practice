b4ca9eef1b011bf5b47f74cdbdedcf87bff39b8e
sourav sahoo <sahoo34sourav@gmail.com>
2023-06-12 22:49:06 +0530

    find greatest number in between two number

---
+++ GreatestNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class GreatestNumber
+ {
+   public static void main(String[]args){
+   Scanner sc = new Scanner (System.in);
+   System.out.println("Enter 1st number - ");
+   int a = sc.nextInt();
+   
+   System.out.println("Enter 2nd number - ");
+   int b = sc.nextInt();
+   
+   if (a > b){
+   System.out.println("a is the greatest number");
+   }
+   else{
+   System.out.println("b is the greatest number");
+   }
+ }
+} 
+
+  
