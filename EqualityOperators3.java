aeb5ff2a492081128f510e6ee03a78f79d2515dd
sourav sahoo <sahoo34sourav@gmail.com>
2023-07-27 20:36:12 +0530

    There should be the relation between arguments type

---
+++ EqualityOperators3.java
@@ -0,0 +1,9 @@
+class EqualityOperators3
+{
+	public static void main(String... args){
+		Object o1 = new Object();
+		Thread s1 = new Thread();
+		String s2 = new String("sourav");
+		System.out.println(o1==s2);
+	}
+}
