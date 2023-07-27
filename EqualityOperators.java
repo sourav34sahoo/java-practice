7eebfdd18c84f715b100afe0d74bc333425a8c14
sourav sahoo <sahoo34sourav@gmail.com>
2023-07-27 20:02:05 +0530

    Equqlity operators for object reference

---
+++ EqualityOperators.java
@@ -0,0 +1,11 @@
+class EqualityOperators
+{
+	public static void main(String... args){
+		Thread t1 = new Thread();
+		Thread t2 = new Thread();
+		Thread t3 = t1;
+		System.out.println(t1==t2);
+	}
+}
+
+		
