2787ae65efda8a0712306369288d9832487202c8
sourav sahoo <sahoo34sourav@gmail.com>
2023-07-27 20:24:14 +0530

    there should not be the relationship between arguments types thats why we get error

---
+++ EqualityOperators2.java
@@ -0,0 +1,9 @@
+class EqualityOperators2
+{
+	public static void main(String... args){
+		object o1 = new object();
+		Thread s1 = new Thread();
+		String s2 = new String("sourav");
+		System.out.println(t1==s2);
+	}
+}
