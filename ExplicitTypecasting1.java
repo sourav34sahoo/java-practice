03b9d08702042bce4c114ba3f4e9b04ab8ad5b03
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-01 16:57:36 +0530

    in Explicit Typecasting

---
+++ ExplicitTypecasting1.java
@@ -0,0 +1,10 @@
+class ExplicitTypecasting1
+{
+	public static void main(String... args){
+		int x = 150;
+		byte b = (byte)x;
+		short s = (short)x;
+		System.out.println(b);
+		System.out.println(s);
+	}
+}
