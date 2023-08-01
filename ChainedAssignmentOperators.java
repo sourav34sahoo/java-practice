0bb517eb3a51a3162c2fd1d5d50681e7896c24bf
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-01 17:48:22 +0530

    Chained Assignment Operators

---
+++ ChainedAssignmentOperators.java
@@ -0,0 +1,11 @@
+class ChainedAssignmentOperators
+{
+	public static void main (String... args){
+		int a,b,c,d;
+		a=b=c=d=20;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
