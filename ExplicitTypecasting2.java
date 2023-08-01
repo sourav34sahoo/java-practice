0808765057a7d8bce35e1ad5fa4c89a4de334a20
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-01 17:10:19 +0530

    when we assign floating point datatype to integral data type,the digit after decimal point will be lossed

---
+++ ExplicitTypecasting2.java
@@ -0,0 +1,10 @@
+class ExplicitTypecasting2
+{
+	public static void main(String... args){
+		double d = 130.456;
+		int a =(int)d;
+		byte b =(byte)d;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
