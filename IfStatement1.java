f8cd20c845aa839d6ac0cd1d2c164d5cf4f7d243
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 16:06:42 +0530

    Here the statement is boolean type

---
+++ IfStatement1.java
@@ -0,0 +1,12 @@
+class ABC
+{
+	public static void main(String... args){
+		boolean b=false;
+		if (b=true){
+			System.out.println("Hello");
+		}
+		else {
+			System.out.println("hii");
+		}
+	}
+}
