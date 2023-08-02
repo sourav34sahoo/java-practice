223018855f6c9b6af08f2d42a31f7c693596218c
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 17:12:20 +0530

    Here expression are allowed for both switch argyment and case label but case lable should be constant expression

---
+++ SwitchStatement3.java
@@ -0,0 +1,12 @@
+class SwitchStatement3
+{
+	public static void main(String... args){
+		int x = 10;
+		switch(x){
+			case 10:
+			     System.out.println("10");
+			case 10+20:
+			     System.out.println("10+20");
+		}
+	}
+}
