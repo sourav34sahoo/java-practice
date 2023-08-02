eafae5bc1fd4d4e6a143dba427d8fb2e5f1b7f62
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 23:50:39 +0530

    use of (final) that the value will be fixed

---
+++ DoWhile3.java
@@ -0,0 +1,13 @@
+class DoWhile3
+{
+	public static void main(String... args){
+		final int a = 10,b = 20;
+		do {
+			System.out.println("hello");
+		}
+		while(a > b);
+		System.out.println("hii");
+	}
+}
+
+		
