de34ee007941058aa3bd0260ab9faa40520c099e
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 21:18:12 +0530

    we can take default case any where within the switch but it is convension to take as last case

---
+++ DefaultCase.java
@@ -0,0 +1,20 @@
+class DefaultCase
+{
+	public static void main(String... args){
+		int x = 3;
+		switch (x)
+		{
+			default:
+			        System.out.println("def");
+			case 0:
+			       System.out.println("0");
+				   break;
+			case 1:
+			       System.out.println("1");
+			case 2:
+			       System.out.println("2");
+		}
+	}
+}
+
+				   
