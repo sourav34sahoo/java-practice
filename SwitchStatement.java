ad22dc5bd6e119c5b313b38fe1d2aed054fe9ba0
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 16:38:06 +0530

    Every case label should be within range of switch argument otherwise we will get compiletime error

---
+++ SwitchStatement.java
@@ -0,0 +1,13 @@
+class SwitchStatement
+{
+	public static void main (String... args){
+		byte b = 10;
+		switch (b)
+		{
+			case 10:
+			     System.out.println("10");
+			case 100:
+			     System.out.println("100");
+		}
+	}
+}
