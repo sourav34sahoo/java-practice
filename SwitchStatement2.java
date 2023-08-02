516f7f538b533701ac54b1bc7f8f1e8cffc47668
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 17:00:17 +0530

    Every case label should be constant expression required

---
+++ SwitchStatement2.java
@@ -0,0 +1,13 @@
+class SwitchStatement2
+{
+	public static void main(String... args){
+		int x = 10;
+		int y = 20;
+		switch (x){
+			case 10:
+			     System.out.println("10");
+			case y:
+			     System.out.println("20");
+		}
+	}
+}
