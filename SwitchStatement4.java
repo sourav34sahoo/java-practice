3e2aee7d40180752860dfd1632991cc1ddc3617f
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 17:18:32 +0530

    duplicate case label are not allowed in switch case

---
+++ SwitchStatement4.java
@@ -0,0 +1,16 @@
+class SwitchStatement4
+{
+	public static void main(String... args){
+		int x = 10;
+		switch(x){
+			case 97:
+			     System.out.println("97");
+			case 98:
+			     System.out.println("98");
+			case 99:
+			     System.out.println("99");
+			case 'a':
+			     System.out.println("a");
+		}
+	}
+}
