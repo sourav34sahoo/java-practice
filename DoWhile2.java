aef70d90b5a9ee1c889cf4bdc8fa608583bd8a5a
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 23:43:02 +0530

    Do while loop

---
+++ DoWhile2.java
@@ -0,0 +1,11 @@
+class DoWhile2
+{
+	public static void main(String... args){
+		int a = 10,b = 20;
+		do {
+			System.out.println("hello");
+		}
+		while(a > b);
+		System.out.println("hii");
+	}
+}
