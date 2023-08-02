d3bcda6164f99c9b2814f2983c2b1f76127c9d8b
sourav sahoo <sahoo34sourav@gmail.com>
2023-08-02 16:03:19 +0530

    Argument to the if statement is boolean type neither it will get compiletime error

---
+++ IfStatement.java
@@ -0,0 +1,12 @@
+class IfStatement
+{
+	public static void main (String... args){
+		int x = 10;
+		if (x == 20){
+			System.out.println("hello");
+		}
+		else {
+			System.out.println("hii");
+		}
+	}
+}
