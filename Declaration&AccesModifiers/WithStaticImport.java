0db76910fc4ea3fc3ee088bfacde4363493e1ddb
sourav sahoo <sahoo34sourav@gmail.com>
2023-10-07 10:38:05 +0530

    With static import

---
+++ Declaration&AccesModifiers/WithStaticImport.java
@@ -0,0 +1,11 @@
+import static java.lang.Math.sqrt;
+import static java.lang.Math.*;
+class WithStaticImport
+{
+
+	public static void main(String...args){
+		System.out.println(sqrt(4));
+		System.out.println(random());
+		System.out.println(max(18,64));
+	}
+}
