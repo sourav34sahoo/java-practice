5e244cd29c7110d0b81ecce248048fc17580319e
sourav sahoo <sahoo34sourav@gmail.com>
2023-10-07 21:23:41 +0530

    Explicit static import will  get priority hence we will get Integer class Max-value is o/p is 2147483647

---
+++ Declaration&AccesModifiers/Purspective.java
@@ -0,0 +1,9 @@
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Byte.*;
+class Purspective
+{
+	public static void main(String...args){
+		System.out.println(MAX_VALUE);
+	}
+}
+ 
