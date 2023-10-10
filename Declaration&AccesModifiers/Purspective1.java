62bd803223a14c8dffc1e7e1fdc1b3a0e5503ff5
sourav sahoo <sahoo34sourav@gmail.com>
2023-10-10 19:24:11 +0530

    The max-value will be considered

---
+++ Declaration&AccesModifiers/Purspective1.java
@@ -0,0 +1,11 @@
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Byte.*;
+class Purspective1
+{
+	static int MAX_VALUE = 999;
+	public static void main(String...args){
+		System.out.println(MAX_VALUE);
+	}
+}
+	
+		
