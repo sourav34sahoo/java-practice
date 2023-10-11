82e0ee46444de14e2c26a31e7d1f48b2cf491a73
sourav sahoo <sahoo34sourav@gmail.com>
2023-10-11 17:41:56 +0530

    the only applicable modifiers are for top level classes are public,<default>,final,abstract,strictfp,if we are using any other we will get compile time error

---
+++ Declaration&AccesModifiers/class modifiers/Modifiers.java
@@ -0,0 +1,13 @@
+private class Modifiers
+{
+	public static void main (String...args){
+		int x = 0;
+		for (int y=0;y<3;y++)
+		{
+            x = x+y;
+		}
+		System.out.println(x);
+	}
+}
+
+            		
