faba05f77568400cf03c82be52dd2c56b0c631db
sourav sahoo <sahoo34sourav@gmail.com>
2023-11-02 11:17:24 +0530

    Encapsulation = Data Hiding+Abstraction

---
+++ Oops Concept/Encapsulation.java
@@ -0,0 +1,15 @@
+class Encapsulation
+{
+	private double balance;
+	public double getbalance()
+	{
+		//VALIDATE USER
+		return balance;
+	}
+		public void setbalance(double balance)
+		{
+			//VALIDATE USER
+			this.balance=balance;
+		}
+}
+
