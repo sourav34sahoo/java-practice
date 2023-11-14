7474f1d91edcc5481d3f04f9e57dd538ac4d5c56
sourav sahoo <sahoo34sourav@gmail.com>
2023-11-14 22:54:04 +0530

    compiletime error

---
+++ Oops Concept/MethodOverridding15.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class MethodOverridding15
+{
+	public void m1()throws IOException
+	{
+	}
+}
+class c extends MethodOverridding15
+{
+	public void m1()throws Exception
+	{
+	}
+}
