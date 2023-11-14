932a98c4208f80249b07eec380c820a3aa1d22b7
sourav sahoo <sahoo34sourav@gmail.com>
2023-11-14 22:49:44 +0530

    valid no compiletime error

---
+++ Oops Concept/MethodOverridding14.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class MethodOverridding14
+{
+	public void m1()throws Exception
+	{
+	}
+}
+class c extends MethodOverridding14
+{
+	public void m1()throws IOException
+	{
+	}
+}
