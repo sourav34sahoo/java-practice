f7dd397bb66e804a3e799dbf1b4ae912907458ab
sourav sahoo <sahoo34sourav@gmail.com>
2024-04-16 20:05:04 +0530

    Arrays can hold only homogenious data element

---
+++ Collection & FrameWork/CollectionArray.java
@@ -0,0 +1,11 @@
+class CollectionArray
+{
+	public static void main(String...args)
+	{
+		CollectionArray[] s = new CollectionArray[10000];
+		s[0] = new CollectionArray();
+		s[1] = new CollectionArray();
+		s[2] = new Customr();
+	}
+}
+
