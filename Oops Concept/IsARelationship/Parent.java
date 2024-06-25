a523ffefe6d79e7d63203f2e718197c4f265c80b
sourav sahoo <sahoo34sourav@gmail.com>
2024-06-25 18:38:00 +0530

    in parent class reference we can call only parent class method we can't called child specific methods

---
+++ Oops Concept/IsARelationship/Parent.java
@@ -0,0 +1,23 @@
+class Parent
+{
+	public void m1()
+	{
+		System.out.println("hii..");
+	}
+}
+class child extends Parent
+{
+	 public void m2()
+	 {
+		 System.out.println("hello");
+	 }
+}
+class Test
+{
+	public static void main(String...args)
+	{
+		Parent p = new Parent();
+		p.m1();
+		p.m2
+	}
+}
