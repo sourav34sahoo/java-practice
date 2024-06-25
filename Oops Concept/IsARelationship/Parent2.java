a7ea5d691ba23a1d6b6e596c3dfb76d4e6f2fc91
sourav sahoo <sahoo34sourav@gmail.com>
2024-06-25 19:16:52 +0530

    parent class reference can be used to hold child class objects by using this reference we can call only parent class method but we can't called child specific methods

---
+++ Oops Concept/IsARelationship/Parent2.java
@@ -0,0 +1,22 @@
+class Parent2
+{
+	public void m1()
+	{
+		System.out.println("hii..");
+	}
+}
+class Child extends Parent2
+{
+	public void m2()
+	{
+		System.out.println("Hello..");
+	}
+}
+class Test2
+{
+	public static void main(String...args)
+	{
+		Parent2 p = new Child();
+		p.m1();
+	}
+}
