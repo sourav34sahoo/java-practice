6ca6d503316a45ac8d94a7bd6600fd0beaafe7cb
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-02 22:03:16 +0530

    in object type array we can provide either declared type or it's child class object

---
+++ array/ArrayObject.java
@@ -0,0 +1,11 @@
+class ArrayObject
+{
+	public static void main(String...args)
+	{
+		Object[] o = new Object[10];
+		o[0]=new Object();
+		o[1]=new Integer(10);
+		o[2]=new Double(10.33);
+		o[3]=new String("sourav");	
+	}
+}
