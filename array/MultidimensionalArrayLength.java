3a437ac6485a522fe266e6dc047f747e1ccd0d3e
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-02 18:25:19 +0530

    in multidimensional arrays length variable represents only base size,but not total size

---
+++ array/MultidimensionalArrayLength.java
@@ -0,0 +1,9 @@
+class MultidimensionalArrayLength
+{
+	public static void main(String...args)
+	{
+		int[][] a = new int[3][2];
+		System.out.println(a.length);
+		System.out.println(a[0].length);
+	}
+}
