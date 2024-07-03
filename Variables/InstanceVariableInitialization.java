17f62ab20ed5389ab411b73d8eb2f9ea5a3bcade
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-03 11:48:46 +0530

    for the instance variable it is not required to perform initialization explicity,jvm will provide default values

---
+++ Variables/InstanceVariableInitialization.java
@@ -0,0 +1,13 @@
+class InstanceVariableInitialization
+{
+	String s;
+	int x;
+	boolean b;
+	public static void main(String...args)
+	{
+		InstanceVariableInitialization i = new InstanceVariableInitialization();
+		System.out.println(i.s);
+		System.out.println(i.x);
+		System.out.println(i.b);
+	}
+}
