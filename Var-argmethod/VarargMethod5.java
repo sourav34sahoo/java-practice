389b089e5e2e2519f989fb5481b3d95bc9f7bcc8
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-05 20:51:27 +0530

    can't declare both m1(int[]) & m1(int...)in test

---
+++ Var-argmethod/VarargMethod5.java
@@ -0,0 +1,14 @@
+class VarargMethod5
+{
+	public static void m1(int[] x)
+	{
+		System.out.println("int[]");
+	}
+	public static void m1(int...x)
+	{
+		System.out.println("int...");
+	}
+	public static void main(String...args)
+	{
+	}
+}
