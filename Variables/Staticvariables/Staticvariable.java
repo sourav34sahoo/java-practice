67027183926f38cd7249f6f54012e52de961545a
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-05 10:42:39 +0530

    static variables can be declare with in the class directly but outside of any method or block or constructor 2-static variable can be accessed either by using class name or object reference 3-within the same class its not required to use class name also we can access directly

---
+++ Variables/Staticvariables/Staticvariable.java
@@ -0,0 +1,11 @@
+class StaticVariable
+{
+	static int x = 10; 
+	public static void main(String...args)
+	{
+		System.out.println(StaticVariable.x);
+		System.out.println(x);
+		StaticVariable s = new StaticVariable();
+		System.out.println(s.x);
+	}
+}
---
+++ Variables/Staticvariables/Staticvariable1.java
@@ -0,0 +1,13 @@
+class StaticVariable1
+{
+	int x = 10;
+	static int y = 20;
+	public static void main(String...args)
+	{
+		StaticVariable1 s = new StaticVariable1();
+		s.x=888;
+		s.y=999;
+		StaticVariable1 s1 = new StaticVariable1();
+		System.out.println(s1.x +","+ s1.y);
+	}
+}
