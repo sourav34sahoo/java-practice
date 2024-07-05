d4f7670e8323349b01ece4ce229d03fb3d40f2fb
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-06 00:56:19 +0530

    it seems to be overriding concept is applicable for static methods but it's not overridding its method hidding

---
+++ MainMethod/MainMethod3.java
@@ -0,0 +1,14 @@
+class MainMethod3
+{
+	public static void main (String...args)
+	{
+		System.out.println("hii");
+	}
+}
+class c extends MainMethod3
+{
+	public static void main(String...args)
+	{
+		System.out.println("Hello");
+	}
+}
