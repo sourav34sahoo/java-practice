8b2c83326f6b389bcf88918ba2030eb9fe8dbaca
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-05 20:44:47 +0530

    in any var-arg method we can take only one var-arg parameter m1(int...x,String...y)

---
+++ Var-argmethod/VarargMethod4.java
@@ -0,0 +1,14 @@
+class VarargMethod4
+{
+	public static void m1(int...x)
+	{
+		System.out.println("var-arg parameter");
+	}
+	public static void m1(String...y)
+	{
+		System.out.println("var-arg parameter");
+	}
+	public static void main(String...args)
+	{
+	}
+}
