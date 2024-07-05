2731fc786bf43c787fffa892091c1f5660bdde53
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-05 12:09:12 +0530

    here the array (i.e object) reference is create but its not initialize (i.e object is not)created so jvm provides null value to the variable a.

---
+++ array/UnInitializeArray.java
@@ -0,0 +1,10 @@
+class UnInitializeArray
+{
+	int []a;
+	public static void main(String...args)
+	{
+		UnInitializeArray u = new UnInitializeArray();
+		System.out.println(u.a);
+		//System.out.println(u.a[0]);
+	}
+}
