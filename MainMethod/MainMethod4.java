c2e6a1aab0182dab61f58d53bc7621124f97a045
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-06 01:07:52 +0530

    overloading concept is applicable for main()but jvm always calls string arguments method only the other method we have to call explicity

---
+++ MainMethod/MainMethod4.java
@@ -0,0 +1,11 @@
+class MainMethod4
+{
+	public static void main(String...args)
+	{
+		System.out.println("hii");
+	}
+	public static void main(int... args)
+	{
+		System.out.println("Hello..");
+	}
+}
