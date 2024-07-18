588acbaae9e395f2e9d90cd28e0403c5c1e93559
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-18 10:33:25 +0530

    every case label should be within range of switch argument type otherwise we will get compile time error

---
+++ if else program/SwitchCase1.java
@@ -0,0 +1,16 @@
+class SwitchCase1
+{
+	public static void main(String...args)
+	{
+		byte b = 10;
+		switch (b)
+		{
+			case 10:
+			System.out.println("10");
+			case 100:
+			System.out.println("100");
+			/*case 1000:
+			System.out.println("1000")*/
+		}
+	}
+}
