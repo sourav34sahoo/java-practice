9a90ed3cfd3dc66fa06cb66665ba3dfac86e7bf4
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-18 11:10:24 +0530

    duplicate case lables are not allowed

---
+++ if else program/SwitchCase5.java
@@ -0,0 +1,16 @@
+class SwitchCase5
+{
+	public static void main(String...args)
+	{
+		int x = 10;
+		switch (x)
+		{
+			case 97:
+			System.out.println("97");
+			case 98:
+			System.out.println("98");
+			case 'a':
+			System.out.println("a");
+		}
+	}
+}
