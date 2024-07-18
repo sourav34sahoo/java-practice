6b7315ae9b63776fe3779e193cdd3c59e7dde861
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-18 12:40:26 +0530

    if we declare variable as final then we get compiletime error find unreachable statemnt

---
+++ loop/WhileLoop1.java
@@ -0,0 +1,25 @@
+class WhileLoop1
+{
+	public static void main (String...args)
+	{
+		int a=10,b=20;
+		while (a<b)
+		{
+			System.out.println("hii");
+		}
+		System.out.println("helo");
+	}
+}
+/*class WhileLoop1
+{
+	public static void main (String...args)
+	{
+		final int a=10,b=20;
+		while (a<b)
+		{
+			System.out.println("hii");
+		}
+		System.out.println("helo");
+	}
+}*/
+//we get compile time error unreachable statement
