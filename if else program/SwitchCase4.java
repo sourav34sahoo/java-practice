869088e4c5baf34b73dc695d53ca8b54cc38ffd8
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-18 11:01:28 +0530

    if we declare y as final then we won't get any compiletime error

---
+++ if else program/SwitchCase4.java
@@ -0,0 +1,15 @@
+class SwitchCase4
+{
+	public static void main(String...args)
+	{
+		int x = 10;
+		final int y=20;
+		switch (x)
+		{
+		case 10:
+		System.out.println("10");
+		case y:
+		System.out.println("20");
+		}
+	}
+}
