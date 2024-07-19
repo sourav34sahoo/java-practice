1237b7054de3f9bc507014c4582a65f1ddda1db9
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-19 12:20:59 +0530

    here we get c.e unreachable statemnt

---
+++ loop/ForLoop4.java
@@ -0,0 +1,12 @@
+class ForLoop3
+{
+	public static void main(String...args)
+	{
+		final int a=10,b=20;
+		for (int i=0;a<b;i++)
+		{
+			System.out.println("hello");
+		}
+		System.out.println("hii");
+	}
+}
