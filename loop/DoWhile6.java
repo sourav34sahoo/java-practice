8b6c85f243bdf59ff23ee7fc59f9d887d86156bc
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-19 11:32:32 +0530

    here first print hii then hello then condition is false loop stop

---
+++ loop/DoWhile6.java
@@ -0,0 +1,13 @@
+class DoWhile6
+{
+	public static void main(String...args)
+	{
+		final int a=10,b=20;
+		do
+		{
+			System.out.println("hii");
+		}
+		while(a>b);
+		System.out.println("hello");
+	}
+}
