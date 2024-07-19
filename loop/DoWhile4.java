f2cf3bca3401c09ea976a713f8f48ba1d869e8d7
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-19 11:25:45 +0530

    while condition is false so first print hello then hii then condition is false loop stop

---
+++ loop/DoWhile4.java
@@ -0,0 +1,13 @@
+class DoWhile4
+{
+	public static void main(String...args)
+	{
+		int a=10,b=20;
+		do
+		{
+			System.out.println("hello");
+		}
+		while(a>b);
+		System.out.println("hii");
+	}
+}
