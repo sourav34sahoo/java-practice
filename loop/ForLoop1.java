6da2b842f459f9f810fca172df127fba363c734f
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-19 11:55:23 +0530

    in the initialization section we can take any valid java statement including sop()also

---
+++ loop/ForLoop1.java
@@ -0,0 +1,11 @@
+class ForLoop1
+{
+	public static void main(String...args)
+	{
+		int i=0;
+		for (System.out.print("hello u r sleeping");i<3;i++)
+		{
+			System.out.println("  no boss u only sleeping");
+		}
+	}
+}
