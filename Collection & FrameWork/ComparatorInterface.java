d9837cd6f7a74108fafaf1af296aea6fc12b7b0e
sourav sahoo <sahoo34sourav@gmail.com>
2024-07-30 18:46:07 +0530

    if we are not passing comparator object then JVM internally calls comparator() which is ment for default natural sorting order these case the o/p is 0,5,10,15,20

---
+++ Collection & FrameWork/ComparatorInterface.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ComparatorInterface
+{
+	public static void main(String...args)
+	{
+		TreeSet t = new TreeSet();
+		t.add(20);
+		t.add(0);
+		t.add(15);
+		t.add(5);
+		t.add(10);
+		System.out.println(t);
+	}
+}
