7554c521057885edca68dc7e8fdbb651e032893a
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-17 18:45:41 +0530

    demo program for sorting elements of list according to default natural sorting order

---
+++ Collection & FrameWork/CollectionClass/CollectionSortDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class CollectionSortDemo
+{
+	public static void main(String...args)
+	{
+		ArrayList l = new ArrayList();
+		l.add("Z");
+		l.add("A");
+		l.add("K");
+		l.add("N");
+		//l.add(new Integer(10));  -->CCE
+		//l.add(null);  -->NPE
+		System.out.println("Before Sorting"+l);
+		Collections.sort(l);
+		System.out.println("After Sorting"+l);
+	}
+}
