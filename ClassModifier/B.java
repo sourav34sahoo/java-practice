e429dd30b56dac49e8f6c68f8922da4c4e527efa
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-22 19:13:06 +0530

    If a class declared as the public Then we can access that class from any where.

--- ClassModifier/B.java
+++ ClassModifier/B.java
@@ -8,3 +8,14 @@ class B
 		a1.m1();
 	}
 }
+
+/*package pack1;
+public class PublicClassDemo
+{
+	public void m1()
+	{
+		System.out.println("Hello");
+	}
+}*/
+
+/* if we are not declaring class PublicClassDemo as public we will get Compile time error while compiling B , saying pack1.PublicClassDemo is not public in pack1; can't be accessed from outside package8*/
