cec8e5f8c32d25bab315f563d6be5c902134d385
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-24 19:43:16 +0530

    program 1 for public members in member modifiers

---
+++ MemberModifiers/PublicMembersDemo3.java
@@ -0,0 +1,10 @@
+package pack4;
+import pack3.PublicMembersDemo2;
+class PublicMembersDemo3
+{
+	public static void main(String...args)
+	{
+		PublicMembersDemo2 pm = new PublicMembersDemo2();
+		pm.m2();
+	}
+}
