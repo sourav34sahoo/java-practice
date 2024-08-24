21c4b472aab3e0a54e4d7ee679d06ca8f411366a
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-24 19:48:45 +0530

    program for public members

---
+++ MemberModifiers/PublicMembersDemo2.java
@@ -0,0 +1,10 @@
+package pack3;
+public class PublicMembersDemo2
+{
+	public void m2()
+	{
+		System.out.println("Hii.");
+	}
+}
+
+// if we declare a member as public then we can access that member from anywhere but coresponding class should be visible (public) i.e before checking member visibility we have to check class visibility. 
