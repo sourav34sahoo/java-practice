146913dc2619fe4e7c46cc7a4436f7e350731085
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-26 10:29:11 +0530

    pack2 program for protected members

---
+++ MemberModifiers/ProtectedMembers/ParentC.java
@@ -0,0 +1,15 @@
+package pack2;
+import pack1.ParentClass;
+class ParentC extends ParentClass
+{
+	public static void main(String...args)
+	{
+		//ParentClass a = new ParentClass();
+		//a.m1();
+		ParentC c = new ParentC();
+		c.m1();
+		//ParentClass a1 = new ParentC();
+		//a1.m1();
+	}
+}
+
