790051f70afa390c7d5c6c043eadb5aff1287074
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-26 11:13:51 +0530

    program for final instance variables

---
+++ MemberModifiers/FinalVariables/FinalInstanceVariables.java
@@ -0,0 +1,15 @@
+class FinalInstanceVariables
+{
+	int x;
+}
+
+/*For the normal instance variables it is not required to perform initialization 
+explicitly JVM will always provide default values.*/
+  
+class FinalInstanceVariables
+{
+	final int x;
+}
+
+/*if the instance variable declare as the final then compulsary we should perform  
+initialization wheather we are using or not otherwise we will get C.E .*/
