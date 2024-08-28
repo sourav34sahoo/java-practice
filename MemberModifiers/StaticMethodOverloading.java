c2251675b17fb165399fa340249e9c5f4514bb26
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-28 10:56:10 +0530

    program for static modifier

---
+++ MemberModifiers/StaticMethodOverloading.java
@@ -0,0 +1,11 @@
+class StaticMethodOverloading
+{
+	public static void main(String...args)
+	{
+		System.out.println("String[]");
+	}
+	public static void main(int[] args)
+	{
+		System.out.println("int[]");
+	}
+}
