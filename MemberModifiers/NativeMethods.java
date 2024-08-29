083d6f33f06a17d6edceb28859c9e689c4edf239
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-29 18:19:52 +0530

    program for native methods

---
+++ MemberModifiers/NativeMethods.java
@@ -0,0 +1,10 @@
+class NativeMethods
+{
+	public native void m1()
+	{
+	}
+}
+
+//native methods declaration should compulsary ends with ";"
+//native methods can't have a body
+//public native void m1();
