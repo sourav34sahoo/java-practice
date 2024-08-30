761498224462171a66101192c4880996a4fbba51
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-30 22:13:23 +0530

    Program for Arrays Retrival

---
+++ Generics/Introduction/ArraysRetrival.java
@@ -0,0 +1,13 @@
+class ArraysRetrival
+{
+	public static void main(String...args)
+	{
+		String[] s = new String[1000];
+		s[0] ="sourav";
+		
+		String name1 = s[0];  // TypeCasting not required.
+	}
+}
+
+/* In case of Arrays at the time of retrival it is not required to
+perform any TypeCasting. */ 
