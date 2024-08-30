e972d4f7b9a593578a29c94bb4c6bc15cda7034a
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-30 11:26:27 +0530

    Program for Extends Vs Implements

---
+++ Interfaces/ExtendsVsImplements.java
@@ -0,0 +1,14 @@
+interface ExtendsVsImplements
+{
+}
+interface B
+{
+}
+interface c extends ExtendsVsImplements,B
+{
+}
+
+/*A class can extend only one class at a time.
+->A class can implement any no.of interfaces at a time.
+->A class can extend a class and can implement any no of interfaces simultanuiously.
+-> An Interface can extend any no of Interfaces at a time. */
