a07e351ef6abf1f523b40be4856ad88b4197baee
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-30 20:49:26 +0530

    Program for Introduction to Generics

--- Generics/Introduction/IntroductionCase1.java
+++ Generics/Introduction/IntroductionCase1.java
@@ -10,6 +10,9 @@ class IntroductionCase1
 		
 		String name1 = (String)l.get(0);
 		String name2 = (String)l.get(1);
-		String name3 = (String)l.get(2);
+		String name3 = (String)l.get(2);  // R.E ->ClassCastException
 	}
 }
+
+/* There is no garente that collection can hold a particular type of objetcs.
+hence w.r.t type collection are not safe to use. */
