6d469a371dc8e1eaf7a1cab9c94b4515d6a5b9a6
sourav sahoo <sahoo34sourav@gmail.com>
2024-08-31 20:31:10 +0530

    Program for Ganerics Conclusion2

---
+++ Generics/Introduction/Conclusion2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Conclusion2
+{
+	public static void main(String...args)
+	{
+		ArrayList<int> l = new ArrayList<int>();C.E required reference , found int
+	}
+}
+/* For the parametertype we can use any Class or Interface name & 
+we can't use premitive type. */ 
