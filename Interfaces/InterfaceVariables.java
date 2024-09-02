a521b301d676da8a54561a8799731aaf1fa279a8
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-02 11:27:07 +0530

    Program for Interface variable

---
+++ Interfaces/InterfaceVariables.java
@@ -0,0 +1,11 @@
+class InterfaceVariables
+{
+	int x = 10;
+}
+
+//Every interface variable is aleays public,static & final wheather we are declaring or not.
+
+/*->public : to make this variable is always available for every implementation class.
+  ->static : Without existing object also implementation class can access this variable.
+  ->final : implementation class can access this variable but can,t modify.
+  */
