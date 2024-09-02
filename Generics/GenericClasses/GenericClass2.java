47858627553ebc6df9c5750a059db48f54e62259
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-02 23:53:09 +0530

    Program for 1.5v a generic version of ArrayList class

---
+++ Generics/GenericClasses/GenericClass2.java
@@ -0,0 +1,7 @@
+class GenericClass2<T>  // T is Type parameter
+{
+	add<T t>
+	t get(int index)
+}
+/* Based on our runtime requirement type parameter 'T' will be replaced with 
+ corresponding provided type. */ 
