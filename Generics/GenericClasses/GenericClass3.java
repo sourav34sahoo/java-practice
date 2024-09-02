d16a194ba0aaff2a0408035d2c5b2075237d5081
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-03 00:00:13 +0530

    Program for loaded version of Arraylist classes

---
+++ Generics/GenericClasses/GenericClass3.java
@@ -0,0 +1,8 @@
+class GenericClass3<String>
+{
+	add(String s)
+	String get(int index)
+}
+
+/*ArrayList<String> l = new ArrayList<String>();
+For this requirment the coresponding loaded version of ArrayList class is */
