f3daa2b00d97ad836955bed76fbf3e251fc15cb9
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-02 10:28:31 +0530

    Program for Interface methods

---
+++ Interfaces/InterfaceMethods.java
@@ -0,0 +1,13 @@
+class InterfaceMethods
+{
+	void m1()  // public & abstract
+	{
+	}
+}
+
+//Whaether we are declaring or not , every interface method is by default public & abstract.
+/* ->void m1();
+   ->public void m1();
+   ->abstract void m1();
+   ->public abstract void m1();
+*/
