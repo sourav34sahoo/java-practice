036e9f2edbef2dc954c4954848de1f11b6a7c0cc
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-03 10:54:47 +0530

    Program for inside implementation classes we can acess interface variables

---
+++ Interfaces/InterfaceVariables2.java
@@ -0,0 +1,12 @@
+interface InterfaceVariables2
+{
+	int x = 10;
+}
+class Test implements InterfaceVariables2
+{
+	public static void main(String[] args)
+	{
+		int x =88;
+		System.out.println(x);
+	}
+}
