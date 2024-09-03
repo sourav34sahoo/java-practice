12c5ad6b6e8acbe5854b20c2265a9e78df3f3653
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-03 17:46:07 +0530

    program for UnBounded types

---
+++ Generics/BoundedTypes/Test.java
@@ -0,0 +1,9 @@
+class Test<T>
+{
+	public static void main(String...args)
+	{
+		Test<Integer> t1 = new Test<Integer>();
+		Test<String> t2 = new Test<String>();
+	}
+}
+//As the type parameter we can pass any type Hence it is UnBounded type.
