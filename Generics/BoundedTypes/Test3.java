dc3e8a294a3e6f0f7c3c9c3899659c19cc476162
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-03 18:53:25 +0530

    Program3 for Bounded type

---
+++ Generics/BoundedTypes/Test3.java
@@ -0,0 +1,6 @@
+class Test3<T extends x>
+{	
+} 
+//x - can be either class or interface
+//if x is a class then as the type parameter we can provide either x type or it's child classes.
+//if x is an interface as the type parameter we can provide either x type or it's implementation classes. 
