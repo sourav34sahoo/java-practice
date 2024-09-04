a894c1716f0e7e89a04309205a6b69ccbf3c9425
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-04 20:33:00 +0530

    Program for valid declaration of Generics Methods & Wild Card Character

--- Generics/GenericMethods&WildCardCharacters/WildCardCharacter.java
+++ Generics/GenericMethods&WildCardCharacters/WildCardCharacter.java
@@ -3,15 +3,24 @@ class WildCardCharacter
 {
 	public static void main(String...args)
 	{
-		//ArrayList<String> l = new ArrayList<String>(); // Valid Declaration
-		//ArrayList<?> l = new ArrayList<String>();  // Valid Declaration 
-		//ArrayList<? extends String> l = new ArrayList<String>(); // Valid Declaration
-		//ArrayList<? super String> l = new ArrayList<String>(); // Valid Declaration
-		//ArrayList<? extends Object> l = new ArrayList<String>();  // Valid Declaration
-		//ArrayList<? extends Number> l = new ArrayList<Integer>(); //Valid Declaration
-		/*ArrayList<? extends Number> l = new ArrayList<String>(); // C.E  incompatible types: 
+		ArrayList<String> l = new ArrayList<String>(); // Valid Declaration
+		
+		ArrayList<?> l = new ArrayList<String>();  // Valid Declaration
+		
+		ArrayList<? extends String> l = new ArrayList<String>(); // Valid Declaration
+		
+		ArrayList<? super String> l = new ArrayList<String>(); // Valid Declaration
+		
+		ArrayList<? extends Object> l = new ArrayList<String>();  // Valid Declaration
+		
+		ArrayList<? extends Number> l = new ArrayList<Integer>(); //Valid Declaration
+		
+		ArrayList<? extends Number> l = new ArrayList<String>(); /* C.E  incompatible types: 
 		ArrayList<String> cannot be converted to ArrayList<? extends Number> */
-		//ArrayList<?> l= new ArrayList<?>(); // C.E  unexpected type
-		ArrayList<?> l = new ArrayList<? extends Number>();
+		
+		ArrayList<?> l= new ArrayList<?>(); // C.E  unexpected type
+		
+		ArrayList<?> l = new ArrayList<? extends Number>(); /*C.E  unexpected type 
+		required: class or interface without bounds, found: ? extends Number
 	}
 }
