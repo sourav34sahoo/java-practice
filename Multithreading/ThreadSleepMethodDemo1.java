7dfbdbab13111da37861713255b9487014dabea5
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-30 19:32:23 +0530

    program1 for Thread sleep()

---
+++ Multithreading/ThreadSleepMethodDemo1.java
@@ -0,0 +1,11 @@
+class ThreadSleepMethodDemo1
+{
+	public static void main(String...args)throws InterruptedException
+	{
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Slide-"+i);
+			Thread.sleep(3000);
+		}
+	}
+}
