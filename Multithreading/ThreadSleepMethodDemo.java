75fa257c172f0501e35147ee76d577c473cc98a7
sourav sahoo <sahoo34sourav@gmail.com>
2024-09-30 17:54:54 +0530

    program for Thread sleep()

---
+++ Multithreading/ThreadSleepMethodDemo.java
@@ -0,0 +1,11 @@
+class ThreadSleepMethodDemo
+{
+	public static void main(String...args)throws InterruptedException
+	{
+		System.out.println("Hii..");
+		Thread.sleep(5000);
+		System.out.println("sourav");
+		Thread.sleep(5000);
+		System.out.println("sahoo");
+	}
+}
