decf56a55980547aeb6da8072f2895a0ddb9029c
sourav sahoo <sahoo34sourav@gmail.com>
2024-10-28 23:19:48 +0530

    program for Thread notify wait concept

---
+++ Multithreading/ThreadA.java
@@ -0,0 +1,28 @@
+class ThreadA
+{
+	public static void main(String...args)throws InterruptedException {
+		ThreadB b = new ThreadB();
+		b.start();
+		synchronized(b) {
+			System.out.println("main thread trying to call wait()");    // 1
+			b.wait();
+			System.out.println("main thread got notification");    // 4
+			System.out.println(b.total);                           // 5
+		}
+	}
+}
+class ThreadB extends Thread
+{
+	int total = 0;
+	public void run()
+	{
+		synchronized(this) {
+			System.out.println("Child thread start notification");    // 2
+			for(int i=1; i<=100; i++){
+				total=total + i;
+			}
+			System.out.println("Child thread trying to given notification");   // 3
+			this.notify();
+		}
+	}
+}
