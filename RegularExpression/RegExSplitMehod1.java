68e21935beff6ce34d00ab1ac0df6bbb1bc32e37
sourav sahoo <sahoo34sourav@gmail.com>
2024-11-21 17:39:10 +0530

    program1 for Split() method

--- RegularExpression/RegExSplitMehod1.java
+++ RegularExpression/RegExSplitMehod1.java
@@ -4,6 +4,15 @@ class RegExSplitMehod1
 	public static void main(String...args)
 	{
 		Pattern p = Pattern.compile("o");
-		String[] s = p.split("Sourav kumar sahoo");
+		String[] s = p.split("Durga Software Solution");
+		for(String s1:s) {
+			System.out.println(s1);
+		}
 	}
 }
+/* O/p
+Durga S
+ftware S
+luti
+n
+*/
