19433f9566b66a90768dd171b05d6dec26c7b7d8
sourav sahoo <sahoo34sourav@gmail.com>
2024-11-27 20:21:50 +0530

    program for Static import

---
+++ Enumeration/StaticImport.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.sqrt;
+import java.util.*;
+class StaticImport
+{
+	public static void main(String...args) {
+		ArrayList al = new ArrayList();
+		System.out.println(sqrt(4));
+	}
+}
