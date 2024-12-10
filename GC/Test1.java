ecee4556dbf4f459a8d41dbfd4b26860ce86c737
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-10 10:23:50 +0530

    program for G.C

---
+++ GC/Test1.java
@@ -0,0 +1,12 @@
+class Test1
+{
+	public static void main(String...args) {
+		Student s = m1();
+	}
+	public static studdent m1()
+	{
+		Student s1 = new Student();
+		Student s2 = new Student();
+		reyurn s1;
+	}
+}
