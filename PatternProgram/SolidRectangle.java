f43c255f7ed615a2580487651fabeea23bf7f999
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-17 19:48:39 +0530

    program for print solid Rectangle pttern

---
+++ PatternProgram/SolidRectangle.java
@@ -0,0 +1,20 @@
+class SolidRectangle
+{
+	public static void main(String...args) {
+		int m = 4;
+		int n = 5;
+		for(int i=1; i<=4; i++){
+			for(int j=1; j<=5; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+	}
+}
+
+/* O/P
+#####
+#####
+#####
+#####
+*/
