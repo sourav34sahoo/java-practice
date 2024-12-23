4c8f949d818fc8c30f7ca50080d394e30c6e919e
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-23 09:49:35 +0530

    program2 for Throws Exception

---
+++ ExceptionHandling/ThrowsDemo2.java
@@ -0,0 +1,9 @@
+class ThrowsDemo2
+{
+	public static void main(String...args) {
+		Thread.sleep(10000);
+	}
+}
+/* unreported exception InterruptedException; must be caught or declared to be thrown
+                Thread.sleep(10000);
+*/
