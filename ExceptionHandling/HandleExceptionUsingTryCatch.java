0dbee04435f962ac4467e1e137b0953af262bda1
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-23 09:54:28 +0530

    program for handle throws exception using try catch

---
+++ ExceptionHandling/HandleExceptionUsingTryCatch.java
@@ -0,0 +1,10 @@
+class HandleExceptionUsingTryCatch
+{
+	public static void main(String...args) {
+		try {
+			Thread.sleep(10000);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
