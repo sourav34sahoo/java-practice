f9c589296ac8e3e1072c138cbdc5b0304a94a0c1
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-24 10:46:16 +0530

    program for try catch case3

---
+++ ExceptionHandling/TryCatchCase3.java
@@ -0,0 +1,17 @@
+class TryCatchCase3
+{
+	public static void main(String...args) {
+		try {
+			System.out.println("hii");
+		}catch(IOException e) {
+			
+		}
+	}
+}
+/* 
+error: cannot find symbol
+                }catch(IOException e) {
+                       ^
+  symbol:   class IOException
+  location: class TryCatchCase3
+*/
