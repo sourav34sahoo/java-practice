f5ae7f3abb174ef5e6e6d151adb2cfeba19a2bb6
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-26 12:33:30 +0530

    program1 for Array

---
+++ array/ArrayDemo1.java
@@ -0,0 +1,12 @@
+class ArrayDemo1
+{
+	public static void main(String...args) {
+		int[] marks = new int[3];
+		marks[0] =97;
+		marks[1] =98;
+		marks[2] =78;
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		System.out.println(marks[2]);
+	}
+}
