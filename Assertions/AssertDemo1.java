2ba6e4e3e30e20be73ef9b7e6b8d4183941d292b
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-29 18:31:05 +0530

    program for Assertions

---
+++ Assertions/AssertDemo1.java
@@ -0,0 +1,12 @@
+class AssertDemo1
+{
+	public static void main(String...args) {
+		int assert = 10;
+		System.out.println(assert);
+	}
+}
+/* O/p
+ javac AssertDemo1.java
+ error: as of release 1.4, 'assert' is a keyword, and may not be used as an identifier
+                System.out.println(assert);
+
