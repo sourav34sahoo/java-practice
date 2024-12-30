3fbd1ac598b0533fb994d4b52a7c4f979d22f8bd
sourav sahoo <sahoo34sourav@gmail.com>
2024-12-30 17:44:29 +0530

    program for Assertion

---
+++ Assertions/AssertDemo2.java
@@ -0,0 +1,17 @@
+class AssertDemo2
+{
+	public static void main(String...args) {
+		int x = 10;
+		;;;;;;;;;;;;;;;;;;;;
+		assert(x == 10);
+		;;;;;;;;;;;;;;;;;;;;
+		System.out.println(x);
+	}
+}
+
+/* O/P
+   javac AssertDemo2.javac
+   java AssertDemo2
+   10
+*/  
+  
