a7116cefc11696a2a80dfc54c8874e4ae2ebdbed
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-05 17:50:00 +0530

    program for String

---
+++ String/StringDemo2.java
@@ -0,0 +1,11 @@
+class StringDemo2
+{
+	public static void main(String...args) {
+		String s1 = new String("spring");
+		s1.concat("fall");
+		String s2 = s1.concat("winter");
+		s2.concat("summer");
+		System.out.println(s1);   // spring
+		System.out.println(s2);   // springwinter
+	}
+}
