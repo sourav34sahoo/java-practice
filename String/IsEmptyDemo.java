7bd12f454d969e29f973bba8ce53656a58ed1ccd
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-07 18:19:39 +0530

    progarm for String isEmpty()

---
+++ String/IsEmptyDemo.java
@@ -0,0 +1,10 @@
+class IsEmptyDemo
+{
+	public static void main(String...args) {
+		String s = "";
+		System.out.println(s.isEmpty());    // true
+		
+		String s1 = "sourav";
+		System.out.println(s1.isEmpty());   // false
+	}
+}
