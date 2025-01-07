2c2cac4d36a0a37041c60c8d27afc0c4276c2c02
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-07 18:30:26 +0530

    program for String length()

---
+++ String/StringLengthDemo.java
@@ -0,0 +1,10 @@
+class StringLengthDemo
+{
+	public static void main(String...args){
+		String s = "sourav";
+		System.out.println(s.length());
+		
+		String s1 = "sahoo";
+		//System.out.println(s1.length);  applicable length variable only in Array
+	}
+}
