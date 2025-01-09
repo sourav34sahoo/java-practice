39e8466a3f79cfe232510d095a265caad3793829
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-09 11:20:36 +0530

    program for String Convert UpperCase And LowerCase

---
+++ String/StringConvertUpperAndLowerCaseDemo.java
@@ -0,0 +1,9 @@
+class StringConvertUpperAndLowerCaseDemo
+{
+	public static void main(String...args) {
+		String s = "AbDIcjdg";
+		System.out.println(s.toUpperCase());   // ABDICJDG
+		
+		System.out.println(s.toLowerCase());   // abdicjdg
+	}
+}
