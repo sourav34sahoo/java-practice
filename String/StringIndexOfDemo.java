535d70475c58f79b43721644024e878e2974e575
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-09 11:02:59 +0530

    program for String indexOf()

---
+++ String/StringIndexOfDemo.java
@@ -0,0 +1,11 @@
+class StringIndexOfDemo
+{
+	public static void main(String...args) {
+		String s ="gubigl";
+		System.out.println(s.indexOf('i'));  // 3
+		
+		System.out.println(s.indexOf('m'));  // -1
+		
+		System.out.println(s.indexOf('g'));  // 0
+	}
+}
