ac401dbed4735d6229ca4b1e28fcd9d35319b29b
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-14 22:49:46 +0530

    program for StringBuffer setLength()

---
+++ String/StringBufferSetLength.java
@@ -0,0 +1,8 @@
+class StringBufferSetLength
+{
+	public static void main(String...args) {
+		StringBuffer sb = new StringBuffer("souravsahoo");
+		sb.setLength(3);
+		System.out.println(sb);   // sou
+	}
+}
