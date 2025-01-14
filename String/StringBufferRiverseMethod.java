64a1f8e658d7ccf303934eda9d7de1bc51fad734
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-14 20:27:57 +0530

    program for StringBuffer riverse()

---
+++ String/StringBufferRiverseMethod.java
@@ -0,0 +1,8 @@
+class StringBufferRiverseMethod
+{
+	public static void main(String...args) {
+		StringBuffer sb = new StringBuffer("sahoo");
+		sb.reverse();
+		System.out.println(sb);   // oohas
+	} 
+}
