2b744d73bcc264257cdbb630e78a854824f07d7a
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-15 11:29:03 +0530

    program for StringBuffer trimToSize()

---
+++ String/StringBufferTrinToSize.java
@@ -0,0 +1,10 @@
+class StringBufferTrimToSize
+{
+	public static void main(String...args) {
+		StringBuffer sb = new StringBuffer(1000);
+		sb.append("ABC");
+		System.out.println(sb.capacity());   // 1000
+		sb.trimToSize();
+		System.out.println(sb.capacity());   // 3
+	}
+}
