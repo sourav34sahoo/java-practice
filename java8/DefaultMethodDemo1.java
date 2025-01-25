0ce6944d8eef88e9d06bd8a07db23ceb370810c0
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-25 13:32:32 +0530

    program for interface implementation

---
+++ java8/DefaultMethodDemo1.java
@@ -0,0 +1,15 @@
+interface I
+{
+	public void m1();
+	public void m2();
+}
+class DefaultMethodDemo1 implements I
+{
+	public void m1(){}
+	public void m2(){}
+}
+class DefaultMethodDemo11 implements I
+{
+	public void m1(){}
+	public void m2(){}
+} 
