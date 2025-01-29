4eafe63280edcbb1cff9aec9ad267cbfad97fbad
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-29 18:29:37 +0530

    program for Predicate and() Method

---
+++ java8/PredicateAndMethod.java
@@ -0,0 +1,20 @@
+import java.util.function.*;
+class PredicateAndMethod
+{
+	public static void main(String...args) {
+		int[] x = {0,5,10,15,20,25,30,35};
+		Predicate<Integer> p1 = e-> e%2==0;
+		Predicate<Integer> p2 = e-> e>10;
+		System.out.println("The numbers which are even and>>10 are:");
+		for(int x1:x) {
+			if(p1.and(p2).test(x1))
+			{
+				System.out.println(x1);
+			}
+		}
+	}
+}
+/* O/P
+20
+30
+*/
