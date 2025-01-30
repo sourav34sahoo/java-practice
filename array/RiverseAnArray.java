9497ef621a7a4ebd966e72bc3df6751daa3532be
sourav sahoo <sahoo34sourav@gmail.com>
2025-01-30 13:44:35 +0530

    program for Riverse an Array

---
+++ array/RiverseAnArray.java
@@ -0,0 +1,9 @@
+class RiverseAnArray
+{
+	public static void main(String...args) {
+		int[] arr= {2,47,29,19};
+		for(int i=arr.length-1; i>=0; i--){
+			System.out.println("Riverse number is :"+arr[i]);
+		}
+	}
+}
