a9d0afe5ca4890fe1a5590dc334db454b7ce4f40
sourav sahoo <sahoo34sourav@gmail.com>
2025-02-12 22:30:42 +0530

    to reprasant quantity of time

--- java8/Streams/PeriodDemo.java
+++ java8/Streams/PeriodDemo.java
@@ -6,6 +6,11 @@ class PeriodDemo
 		LocalDate today = LocalDate.now();
 		Period p = Period.between(birthday,today);
 		System.out.printf("Age is %dYears %d Months %d Days",p.getYears(),p.getMonths(),p.getDays());
+		
+		LocalDate deathday = LocalDate.of(1989+80,06,15);
+		Period p1 = Period.between(birthday,deathday);
+		int d = p1.getYears()*365+p1.getMonths()*30+p1.getDays();
+		System.out.printf("/n you will be on the earth only %d Days..Hurry up to do more important Things ",d);
 	}
 }
 /* O/P
