e352097daf123e6b53db9cc6c2e0521fa59937cd
sourav sahoo <sahoo34sourav@gmail.com>
2025-02-12 19:27:22 +0530

    program to get both LocalDateAndTime

---
+++ java8/Streams/BothLocalDateAndTimeDemo4.java
@@ -0,0 +1,11 @@
+import java.time.*;
+class BothLocalDateAndTimeDemo4
+{
+	public static void main(String...args) {
+		LocalDateTime dt =LocalDateTime.now();
+		System.out.println(dt);
+	}
+}
+/*O/P
+2025-02-12T19:24:31.497698300
+*/
