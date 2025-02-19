3c7d2e8d2f2d3f9affe5b8af863511891769c4c4
sourav sahoo <sahoo34sourav@gmail.com>
2025-02-19 18:00:29 +0530

    program for Variable declare as transient final

---
+++ Serialization/TransientFinal.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class Dog implements Serializable
+{
+ 	 transient final int i=10;   
+	 transient int j=20;   
+}
+class TransientFinal
+{
+	public static void main(String...args)throws Exception {
+		Dog d1 = new Dog();
+		
+		FileOutputStream fos = new FileOutputStream("def.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(d1);
+		
+		FileInputStream fis = new FileInputStream("def.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Dog d2 = (Dog)ois.readObject();
+		
+		System.out.println(d2.i +"....."+ d2.j);   // 10.....0
+	}
+}
