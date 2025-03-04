8f2c4938ebe1f029ddbbee03259871dcc0c80452
sourav sahoo <sahoo34sourav@gmail.com>
2025-03-04 14:27:28 +0530

    program for Linear search

--- LogicalPrograming/LinearSearch.java
+++ LogicalPrograming/LinearSearch.java
@@ -2,11 +2,16 @@ class LinearSearch
 {
 	public static void main(String...args) {
 		int []a = {3,5,2,4,6};
-		int k = 4;
+		int k = 8;
+		int temp = 0;
 		for(int i=0;i<a.length; i++){
 			if(a[i]==k){
 				System.out.println("Element is present at "+i+" position");
-			}
+				temp = temp+1;
+			} 
+		}
+		if(temp == 0){
+			System.out.println("element is not available");
 		}
 	}
 }
